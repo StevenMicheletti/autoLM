1d276ca0f92b93f83ba1622829ac07bef04ad7ec
Steven Micheletti <32178883+StevenMicheletti@users.noreply.github.com>
2020-06-23 17:00:32 -0700

    Update run_autolm.sh

--- run_autolm.sh
+++ run_autolm.sh
@@ -3,7 +3,7 @@
 OUTPUT=autolm.out
 FREQ_FILE=all_freq.txt
 ENV_FILE=env.txt
-COORDS_FILE=locations.txt #remove if 
+COORDS_FILE=locations.txt #put NA if not using
 STANDARDIZE=TRUE
 
 Rscript autoLM.R $OUTPUT $ENV_FILE $FREQ_FILE $STANDARDIZE $COORDS_FILE
